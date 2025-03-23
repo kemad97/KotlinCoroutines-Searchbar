ee005bcf92e62e50c894cea6d16e624ac5d64c2c
kemad97 <kerolos.emad992@gmail.com>
2025-03-23 21:01:37 +0200

    .

--- app/src/main/java/com/example/kotlincoroutinessearchbar/MainActivity.kt
+++ app/src/main/java/com/example/kotlincoroutinessearchbar/MainActivity.kt
@@ -3,45 +3,58 @@ package com.example.kotlincoroutinessearchbar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.kotlincoroutinessearchbar.ui.theme.KotlinCoroutinesSearchbarTheme
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            KotlinCoroutinesSearchbarTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+        setContent { SearchScreen() }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchScreen() {
+
+    var text by remember { mutableStateOf("") }
+    var active by remember { mutableStateOf(false) }
+
+    val itemList = listOf("Alice", "Bob", "Kerolos", "Ali", "Ahmed")
+
+    val filteredList by remember(text) {
+        mutableStateOf(itemList.filter { it.contains(text, ignoreCase = true) })
+    }
+
+
+
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+        SearchBar(
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
+            query = text,
+            onQueryChange = { text = it },
+            onSearch = { active = false },
+            active = active,
+            onActiveChange = { active = it },
+            placeholder = { Text("Search") }
+        )
+        {
+            LazyColumn(modifier = Modifier.fillMaxSize().padding(top = 16.dp)) {
+                items(filteredList) { item ->
+                    Text(text = item, modifier = Modifier.padding(8.dp))
                 }
             }
+
         }
+
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    KotlinCoroutinesSearchbarTheme {
-        Greeting("Android")
-    }
-}
